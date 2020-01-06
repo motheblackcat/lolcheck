2ce306e75705adf02835045ebc27c0c489e79dc6
Mo <morad.elhamdouni@gmail.com>
2020-01-06 12:44:36 +0100

    Updates

---
+++ precache-manifest.7e6ae65c94c0c89573fec9f161573a2a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b386701876f2159258e076d17c49038",
+    "url": "/lolcheck/index.html"
+  },
+  {
+    "revision": "4136bb9038d22c409202",
+    "url": "/lolcheck/static/css/main.1b91fc82.chunk.css"
+  },
+  {
+    "revision": "c5b28502e34595dd60fb",
+    "url": "/lolcheck/static/js/2.ae4814c6.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/lolcheck/static/js/2.ae4814c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4136bb9038d22c409202",
+    "url": "/lolcheck/static/js/main.47adf85d.chunk.js"
+  },
+  {
+    "revision": "caf4be796586f44f6fce",
+    "url": "/lolcheck/static/js/runtime-main.eaa60b38.js"
+  },
+  {
+    "revision": "39964f7f7440521c5d53a63cbeed96ff",
+    "url": "/lolcheck/static/media/bronze.39964f7f.png"
+  },
+  {
+    "revision": "4aaf9754594ca11cbbe6a146ffb0eaa1",
+    "url": "/lolcheck/static/media/challenger.4aaf9754.png"
+  },
+  {
+    "revision": "c901832766fb4f856d1f9603208c6761",
+    "url": "/lolcheck/static/media/diamond.c9018327.png"
+  },
+  {
+    "revision": "19be519b965d28bf5af7cbb6b7f2471a",
+    "url": "/lolcheck/static/media/gold.19be519b.png"
+  },
+  {
+    "revision": "d0d4b62f7415cf0ca06d60ce1ae36744",
+    "url": "/lolcheck/static/media/grandmaster.d0d4b62f.png"
+  },
+  {
+    "revision": "1a6b4669f3cb62c2edf32971da0b146e",
+    "url": "/lolcheck/static/media/iron.1a6b4669.png"
+  },
+  {
+    "revision": "4156f1c160999db19a69d5bb0e2aaca7",
+    "url": "/lolcheck/static/media/master.4156f1c1.png"
+  },
+  {
+    "revision": "86ac5170561afe99a457dccd664eaf72",
+    "url": "/lolcheck/static/media/platinum.86ac5170.png"
+  },
+  {
+    "revision": "3a4aa1a5e576ae0ffb79db1f7505ba37",
+    "url": "/lolcheck/static/media/silver.3a4aa1a5.png"
+  },
+  {
+    "revision": "eec1564a1eb8ca3e8618aedc2cdf442b",
+    "url": "/lolcheck/static/media/unranked.eec1564a.png"
+  }
+]);
